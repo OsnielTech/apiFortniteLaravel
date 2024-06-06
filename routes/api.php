0310bda57a02d1129bc22f73176878fafc24d83e
Osniel Tech <osnieltech@gmail.com>
2024-06-05 22:01:02 -0500

    primer commit

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/students', function () {
+    return 'Obteniendo lista de estudiantes';
+});
+Route::get('/students/{id}', function () {
+    return 'Obteniendo estudiante';
+});
+
+
+Route::post('/students', function () {
+    return 'Creando estudiante';
+});
+
+Route::put('/students/{id}', function () {
+    return 'Actualizar estudiante';
+});
+Route::delete('/students/{id}', function () {
+    return 'Eliminando estudiante';
+});
